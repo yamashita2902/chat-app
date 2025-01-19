7cffcbe2f1c7e9ca9b28eda4606f05d0d596bd76
yamashita2902 <yptkyyp57575@gmail.com>
2025-01-20 01:12:07 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2025_01_19_153317) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
