1b0f25f3a73124c538ae3b59600feb35e75216cc
yamashita2902 <yptkyyp57575@gmail.com>
2025-01-21 23:29:31 +0900

    room_usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_20_143521) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_21_135358) do
   create_table "rooms", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms_users", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_rooms_users_on_room_id"
+    t.index ["user_id"], name: "index_rooms_users_on_user_id"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
@@ -30,4 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_20_143521) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rooms_users", "rooms"
+  add_foreign_key "rooms_users", "users"
 end
